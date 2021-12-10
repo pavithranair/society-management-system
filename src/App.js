7342b26b2c64b662d77c3fa9bb39927a08bba1f3
Pavithra Nair <pmpavithranair@gmail.com>
2021-12-10 22:11:28 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import ContactUs from './pages/ContactUs/ContactUs'
+import Post from './pages/Post/Post'
+import Signup from './pages/Signup/Signup'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="post" element={<Post />} />
+          <Route path="signup" element={<Signup />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/ContactUs/ContactUs.js
@@ -0,0 +1,9 @@
+import './ContactUs.css'
+
+export default function ContactUs() {
+    return (
+        <div>
+            Contact Us
+        </div>
+    )
+}
